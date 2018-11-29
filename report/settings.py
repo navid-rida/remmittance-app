29cf38413d91072badd577cd771a764f187bd527
Navid Rida <navid.anzum88@gmail.com>
2018-11-29 15:16:52 +0600

    Uploading setting

--- report/settings.py
+++ report/settings.py
@@ -129,6 +129,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 MEDIA_URL = '/media/'
+
 #User registration and Authentication
 
 LOGOUT_REDIRECT_URL='login'
