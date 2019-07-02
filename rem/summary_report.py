8de8cc6638056c674c5e24321b1c223a4847f8df
Navid Rida <navid.anzum88@gmail.com>
2019-07-02 19:05:53 +0600

    Summary rep update 3

--- rem/summary_report.py
+++ rem/summary_report.py
@@ -6,7 +6,7 @@ from .DataModels import *
 
 year = timezone.now().date().year
 
-def group_branchwise(year,month, ex_column=False):
+def group_branchwise(year,month, ex_column=False, house=None):
     """ Returns number and sum of
     remittance for each branch"""
     if ex_column:
@@ -25,3 +25,13 @@ def get_total_branch_summary(year, month):
     #q = Remmit.objects.filter(date_create__year=year,date_create__month=month)
     df = group_branchwise(qset=q)
     return df
+
+def get_ex_wise_branch_summary():
+    year = '2019'
+    month = 6
+    exc = ExchangeHouse.objects.all()
+    dc = dict()
+    for house in exc:
+        name=house.name
+        df = group_branchwise(year=year,month=month,ex_column=True,house=house)
+        dc[name] = df
