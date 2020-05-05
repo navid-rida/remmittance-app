8984bf1ffa79c2af14ee5349ae995becb8865112
navid-rida <navid.anzum88@gmail.com>
2020-05-05 12:18:30 +0600

    Receiver edit permission change

--- rem/rule_set.py
+++ rem/rule_set.py
@@ -12,8 +12,8 @@ def is_same_branch_user(user,remittance):
     return remittance.branch == user.employee.branch
 
 @rules.predicate
-def is_same_branch_user(user,remittance):
-    return remittance.branch == user.employee.branch
+def is_same_branch_as_creator(user,receiver):
+    return receiver.created_by.employee.branch == user.employee.branch
 
 @rules.predicate
 def is_transaction_hour(user):
@@ -51,7 +51,7 @@ rules.add_perm('rem.can_mark_paid_remittance', is_ho_settlement_user | (is_entry
 
 #---------------------------------------------------------------------------------
 
-rules.add_perm('rem.change_reciver', is_same_branch_user | rules.is_superuser )
+rules.add_perm('rem.change_reciver', is_same_branch_as_creator | rules.is_superuser )
 rules.add_perm('rem.allow_if_transaction_hour', is_transaction_hour )
 
 #---------------------------------------------------------------------------------
