10eedb5ca6e0fa218ce60954f9cd4f32aaf4e02b
Navid Rida <navid.anzum88@gmail.com>
2020-06-14 17:18:28 +0600

    Sub branch code edit

--- rem/data_migrations.py
+++ rem/data_migrations.py
@@ -1,2 +1,14 @@
-from .models import Receiver
+from .models import *
 from django.contrib.auth.models import User
+import pandas as pdb
+from pathlib import Path
+
+
+df = pd.read_excel(Path('F:\\Projects\\Remmittance web app\\code change\\cdcng.xlsx'), dtype=object)
+
+def change_sub_branch_code(booth, df):
+    for i,r in df.iterrows():
+        if booth.code == r['pn']:
+            booth.code = r['new']
+            booth.save()
+            print(r['new'], booth.code, booth.name)
