c53e75e8a3f61dfe3a4faf044b20fd5511dd4168
Navid Rida <navid.anzum88@gmail.com>
2021-04-01 17:55:34 +0600

    Signal debug for mark settle

--- rem/signals.py
+++ rem/signals.py
@@ -14,8 +14,8 @@ def create_cash_incentive(sender, instance, created, **kwargs):
             CashIncentive.objects.create(remittance = instance, cash_incentive_amount= 0, entry_category=instance._entry_cat, unpaid_cash_incentive_reason=instance._reason_a)
         else:
             pass
-    else:
+    """else:
         #This section should be removed after implementing seperate Cash Incentive Table
-        if instance._entry_cat=='P' and (not instance.check_if_cash_incentive_paid()):
-            CashIncentive.objects.create(remittance = instance, cash_incentive_amount= instance.calculate_cash_incentive(), date_cash_incentive_paid=timezone.now(), entry_category=instance._entry_cat )
+        if instance._entry_cat and instance._entry_cat=='P' and (not instance.check_if_cash_incentive_paid()):
+            CashIncentive.objects.create(remittance = instance, cash_incentive_amount= instance.calculate_cash_incentive(), date_cash_incentive_paid=timezone.now(), entry_category=instance._entry_cat )"""
         
