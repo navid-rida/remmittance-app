4dcca836ad57855db2b00743e0a428582c3f4a53
Navid Rida <navid.anzum88@gmail.com>
2021-04-05 12:00:36 +0600

    Notice for time change

--- rem/rule_set.py
+++ rem/rule_set.py
@@ -21,7 +21,7 @@ def is_same_branch_as_creator(user,receiver):
 
 @rules.predicate
 def is_transaction_hour(user):
-    return time(9,59)<timezone.localtime().time()<time(12,40)
+    return time(9,59)<timezone.localtime().time()<time(12,45)
 
 @rules.predicate
 def remittance_less_than_usd1500(user,remittance):
